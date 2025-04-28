167e2126973067a1420808cb1e508fa020ee2be7
Kamalesh <kamalesh2023@gmail.com>
2025-04-29 00:32:54 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function App() {
+  return (
+    <div>
+      Initial Setup of Frontend qna bot vite + react + tailwind + shadcn
+    </div>
+  );
+}
+
+export default App;
